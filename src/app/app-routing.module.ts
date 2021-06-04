f1decf538394f68df60477081e6e6f3b389d1844
giacomomangelli <giacomomsidp@gmail.com>
2021-06-04 15:18:44 +0200

    sistemazione styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutomobileCreateComponent } from './automobile/automobile-create/automobile-create.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomeComponent },
-  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: 'automobile/create', component: AutomobileCreateComponent },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ];
 
 @NgModule({
